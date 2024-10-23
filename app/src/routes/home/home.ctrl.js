a7f882b6ff2eec5fb63b196ca2925eac3bcdb792
Blowhooo <blowhooo@gmail.com>
2024-10-23 20:38:36 +0900

    프런트 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -7,15 +7,35 @@ const output = {
   login : (req, res) => {
     res.render("home/login");
   },
-}
+};
+
+const users = {
+  id: ["testyjh1", "testyjh2", "testyjh3"],
+  psword : ['1234', "5678", "9101"]
+};
 
 const process = {
   login : (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+          psword = req.body.psword;
+
+    if(users.id.includes(id)){
+      const idx = users.id.indexOf(id);
+      if(users.psword[idx] === psword){
+        return res.json({
+          "success" : true,
+        })
+      }
+    }
+
+    return res.json({
+      "success" : false,
+      "msg" : "로그인에 실패하셨습니다.",
+    })
   }
-}
+};
 
 module.exports = {
   output,
   process
-}
+};
